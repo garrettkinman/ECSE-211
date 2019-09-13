23070d43a1a77b16b739c810d96c5754c4b0f285
emmacona <emmanuelle.coutu-nadeau@mail.mcgill.ca>
2019-09-13 13:12:31 -0400

    Updated bang bang class

--- src/ca/mcgill/ecse211/lab1/BangBangController.java
+++ src/ca/mcgill/ecse211/lab1/BangBangController.java
@@ -4,6 +4,8 @@ import static ca.mcgill.ecse211.lab1.Resources.*;
 
 public class BangBangController extends UltrasonicController {
 
+  public static int TOO_CLOSE = 50;
+
   public BangBangController() {
     LEFT_MOTOR.setSpeed(MOTOR_HIGH); // Start robot moving forward
     RIGHT_MOTOR.setSpeed(MOTOR_HIGH);
@@ -13,36 +15,49 @@ public class BangBangController extends UltrasonicController {
 
   @Override
   public void processUSData(int distance) {
+    /**
+     * 1. If robot is within range, just keep both motors fast.
+     * 2. If robot is too close, make left motor faster and right motor slower to turn right.
+     *    If robot is really too close, set both motor speed to fast and move backwards.
+     * 3. If robot is too far from the wall, right motor faster and left slower to turn to left.
+     * 
+     */
+
+    // Check to see if magnitude is within bounds
     filter(distance);
 
+    // Initialize speed variables for both motors
     int leftMotorSpeed = 0;
     int rightMotorSpeed = 0;
 
-    if (distance < BAND_CENTER - BAND_WIDTH) { // Robot is too close to wall
-      if (distance < 30) { // Robot is really close to wall
-        LEFT_MOTOR.setSpeed(MOTOR_HIGH);
-        RIGHT_MOTOR.setSpeed(MOTOR_HIGH);
-        LEFT_MOTOR.forward();
-        RIGHT_MOTOR.backward(); // Reverse right wheel to turn faster
-        return;
-      }
-
-      leftMotorSpeed = MOTOR_HIGH;
-      rightMotorSpeed = MOTOR_LOW;
-    } else if (distance > BAND_CENTER + BAND_WIDTH) { // If too far from wall
-      leftMotorSpeed = MOTOR_LOW;
-      rightMotorSpeed = MOTOR_HIGH;
-    } else { // Within range, so move forward
+    // 1. Robot getting too close to wall
+    if (distance <= BAND_WIDTH) {
       leftMotorSpeed = MOTOR_HIGH;
       rightMotorSpeed = MOTOR_HIGH;
     }
+    else if (distance < BAND_CENTER - BAND_WIDTH) {
+      if (distance < TOO_CLOSE) {
+        LEFT_MOTOR.setSpeed(MOTOR_HIGH);
+        RIGHT_MOTOR.setSpeed(MOTOR_HIGH);
+        LEFT_MOTOR.forward();
+        RIGHT_MOTOR.backward();
+        return;
+      }
+      leftMotorSpeed = MOTOR_HIGH;
+      rightMotorSpeed = MOTOR_LOW;
+    }
+    else {
+      leftMotorSpeed = MOTOR_LOW;
+      rightMotorSpeed = MOTOR_HIGH;
+    }
 
-    // Move robot forward
+    // Set speed of motors
     LEFT_MOTOR.setSpeed(leftMotorSpeed);
     RIGHT_MOTOR.setSpeed(rightMotorSpeed);
+    // Make robot move forward
     LEFT_MOTOR.forward();
     RIGHT_MOTOR.forward();
-  }// TODO: process a movement based on the us distance passed in (BANG-BANG style)
+  }
 
 
   @Override
